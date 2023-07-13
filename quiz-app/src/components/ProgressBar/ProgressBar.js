f86a91be0f685fbc24971ee0cc7bdf3bc1118d6a
Suruthi-13 <suruthi.s@disprz.com>
2023-07-13 12:19:56 +0530

    Fourth-Commit

--- quiz-app/src/components/ProgressBar/ProgressBar.js
+++ quiz-app/src/components/ProgressBar/ProgressBar.js
@@ -1,29 +1,30 @@
 import React,{useState} from 'react';
 // import {CiCircleCheck } from 'react-icons/fa';
-import './ProgressBar.css'
-
+import './ProgressBar.css';
+import { QUIZ_QUESTIONS } from '../../Constants/QuizQuestionConstants';
+import { AiOutlineTrophy } from "react-icons/ai";
 import {TiTick,TiTimes} from "react-icons/ti";
 
 function ProgressBar() {
     const items=["Q1","Q2","Q3"]
     const [currentStep,setCurrentStep]=useState(1);
     const [isComplete,setIsComplete] = useState(false);
-    const [isAnsweredCorrectly,setIsAnsweredCorrectly]=useState(false);
+    const [isAnsweredCorrectly,setIsAnsweredCorrectly]=useState(true);
   return (
     <>
     <div class='progress-bar'>
-      {items.map((item,i)=>(
-        <div key={i} className={`step-item ${currentStep === i+1 && "active"} ${ i+1 < currentStep && "complete"} ${isAnsweredCorrectly && 'incorrect'}`}>
-        <div className='step-index'>{i+1 < currentStep || isComplete ? (isAnsweredCorrectly?<TiTick />:<TiTimes/>) :  i+1 }</div>
-        <div className='empty-line'><hr/></div>
-        <p className='step'>{item}</p>
+      {QUIZ_QUESTIONS.map((item,i)=>(
+        <div key={i} className={`step-item ${currentStep === i+1 && "active"} ${ (i+1 < currentStep || isComplete) && "complete"} ${isAnsweredCorrectly && 'incorrect'}`}>
+        <div className='step-index'>{i+1 < currentStep || isComplete ? (isAnsweredCorrectly?<AiOutlineTrophy />:<TiTimes/>) :  i+1 }</div>
+        {/* <div className='empty-line'><hr/></div> */}
+        {/* <p className='step'>{item}</p> */}
         </div>
       ))}
     </div>
-    <button className='btn' onClick={()=>{ 
-        currentStep === items.length ? setIsComplete(true):
+    <div className='button-container'><button className='btn' onClick={()=>{ 
+        currentStep === QUIZ_QUESTIONS.length ? setIsComplete(true):
         setCurrentStep((prev)=>prev+1)
-        }}>{currentStep === items.length ? "FINISH" : "NEXT"}</button>
+        }}>{currentStep === QUIZ_QUESTIONS.length ? "FINISH" : "NEXT"}</button></div>
     </>
   );
 }
